625fda3686f1a789e54b4e22c4fe4e59acbd8384
tanmaygar <72341248+tanmaygar@users.noreply.github.com>
2021-03-27 09:23:07 +0530

    Add files via upload

--- Assignment4/codes/GATE-2015(CS-SET-3)-Q37.py
+++ Assignment4/codes/GATE-2015(CS-SET-3)-Q37.py
@@ -6,6 +6,7 @@ from scipy.stats import bernoulli
 from scipy.stats import norm
 from scipy.stats import binom
 
+
 #number of test cases
 k = 100000
 #A = (Y = 0 and X3 = 0)
@@ -40,17 +41,32 @@ print("The probability is: ", probab)
 
 
 #plotting
+#plotting
 
 cases = ['']
 
 probab_theo = 0.75
 probab_sim = probab
 
+#plt.figure(figsize=(24,16))
+#plt.subplots_adjust(left = None, bottom = 0, right = None, top = 1, wspace = None, hspace = None )
 x = np.arange(len(cases))
 plt.bar(x + 0.00, probab_theo, color = 'b', width = 0.2, label = 'Theoretical')
 plt.bar(x + 0.2, probab_sim, color = 'g', width = 0.2, label = 'Simulated')
 plt.xlabel('Theoretical v/s Simulated')
 plt.ylabel('Probability')
 plt.xticks(x  + 0.2/2,[''])
+
+
+a = np.arange(0,0.76,0.001)
+plt.yticks(a)
+plt.ylim([0.74,0.76])
+#plt.margins(0.01)
+plt.grid(b = True, color ='black',
+        linestyle ='-.', linewidth = 0.5,
+        alpha = 0.2)
+
 plt.legend()
 plt.show()
+
+
